10298551467d2a3501dbbe42cab3ec5553737083
anilak222 <k.anastasov034@gmail.com>
2024-08-15 02:03:09 +0100

    Add files via upload

---
+++ ConfigHUDController.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections;
+using Comfort.Common;
+using EFT;
+using EFT.CameraControl;
+using EFT.UI;
+using GPUInstancer;
+using HarmonyLib;
+using JetBrains.Annotations;
+using UnityEngine;
+
+namespace ConfigHUD
+{
+    internal class ConfigHUDController : MonoBehaviour
+    {
+
+        public void Initialize(Transform stances, Transform stanceslider, Transform volumeslider, Transform volumeicon)
+        {
+            this._battleStanceTransform = stances.gameObject;
+            this._battleStanceSliderTransform = stanceslider.gameObject;
+            this._battleVolumeSliderTransform = volumeslider.gameObject;
+            this._battleVolumeIconTransform = volumeicon.gameObject;
+            this._battleStanceTransform.SetActive(ConfigHUDPlugin.stanceVisibility.Value);
+            this._battleStanceSliderTransform.SetActive(ConfigHUDPlugin.stanceVisibility.Value);
+            this._battleVolumeSliderTransform.SetActive(ConfigHUDPlugin.volumeVisibility.Value);
+            this._battleVolumeIconTransform.SetActive(ConfigHUDPlugin.volumeVisibility.Value);
+            ConfigHUDPlugin.stanceVisibility.SettingChanged += (sender, arguments) =>
+            {
+                this._battleStanceTransform.SetActive(ConfigHUDPlugin.stanceVisibility.Value);
+                this._battleStanceSliderTransform.SetActive(ConfigHUDPlugin.stanceVisibility.Value);
+            };
+            ConfigHUDPlugin.volumeVisibility.SettingChanged += (sender, arguments) =>
+            {
+                this._battleVolumeSliderTransform.SetActive(ConfigHUDPlugin.volumeVisibility.Value);
+                this._battleVolumeIconTransform.SetActive(ConfigHUDPlugin.volumeVisibility.Value);
+            };
+        }
+
+        public void Initialize2(Transform firemodetext)
+        {
+            this._firemodeTextTransform = firemodetext.gameObject;
+            this._firemodeTextTransform.SetActive(ConfigHUDPlugin.ammotextVisibility.Value);
+            ConfigHUDPlugin.ammotextVisibility.SettingChanged += (sender, arguments) =>
+            {
+                this._firemodeTextTransform.SetActive(ConfigHUDPlugin.ammotextVisibility.Value);
+            };
+        }
+
+        public void Update()
+        {
+            if (ConfigHUDPlugin.checkAmmo.Value.IsDown())
+            {
+                ToggleFireModeText();
+            }
+        }
+
+        private void ToggleStanceHUD()
+        {
+            if (this.GetLocalPlayerFromWorld() == null)
+            {
+                return;
+            }
+            ConfigHUDPlugin.stanceVisibility.Value = !ConfigHUDPlugin.stanceVisibility.Value;
+            this._battleStanceTransform.transform.gameObject.SetActive(_stanceVisibility);
+            this._battleStanceSliderTransform.transform.gameObject.SetActive(_stanceVisibility);
+            this._battleStanceTransform.SetActive(ConfigHUDPlugin.stanceVisibility.Value);
+            this._battleStanceSliderTransform.SetActive(ConfigHUDPlugin.stanceVisibility.Value);
+        }
+        private void ToggleVolumeHUD()
+        {
+            if (this.GetLocalPlayerFromWorld() == null)
+            {
+                return;
+            }
+            ConfigHUDPlugin.volumeVisibility.Value = !ConfigHUDPlugin.volumeVisibility.Value;
+            this._battleVolumeSliderTransform.transform.gameObject.SetActive(_volumeVisibility);
+            this._battleVolumeIconTransform.transform.gameObject.SetActive(_volumeVisibility);
+            this._battleVolumeSliderTransform.SetActive(ConfigHUDPlugin.volumeVisibility.Value);
+            this._battleVolumeIconTransform.SetActive(ConfigHUDPlugin.volumeVisibility.Value);
+        }
+
+        private void ToggleFireModeText()
+        {
+            if (this.GetLocalPlayerFromWorld() == null)
+            {
+                return;
+            }
+            ConfigHUDPlugin.ammotextVisibility.Value = !ConfigHUDPlugin.ammotextVisibility.Value;
+            this._firemodeTextTransform.gameObject.SetActive(_firemodeVisibility);
+            this._firemodeTextTransform.SetActive(ConfigHUDPlugin.ammotextVisibility.Value);
+        }
+
+        private Player GetLocalPlayerFromWorld()
+        {
+            GameWorld instance = Singleton<GameWorld>.Instance;
+            if (instance == null || instance.MainPlayer == null)
+            {
+                return null;
+            }
+            return instance.MainPlayer;
+        }
+
+        private GameObject _battleStanceTransform;
+
+        private GameObject _battleStanceSliderTransform;
+
+        private GameObject _battleVolumeSliderTransform;
+
+        private GameObject _battleVolumeIconTransform;
+
+        private GameObject _firemodeTextTransform;
+
+        private bool _stanceVisibility;
+
+        private bool _volumeVisibility;
+
+        private bool _firemodeVisibility;
+    }
+}
---
+++ ConfigHUDPatch2.cs
@@ -0,0 +1,23 @@
+﻿using EFT;
+using EFT.UI;
+using HarmonyLib;
+using JetBrains.Annotations;
+using SPT.Reflection.Patching;
+using System.Reflection;
+using UnityEngine;
+
+namespace ConfigHUD
+{
+    public class ConfigHUDPatch2 : ModulePatch
+    {
+        protected override MethodBase GetTargetMethod() => AccessTools.Method(typeof(EftBattleUIScreen), nameof(EftBattleUIScreen.Show), new System.Type[] { typeof(GamePlayerOwner) });
+
+        [PatchPostfix]
+        public static void Postfix(EftBattleUIScreen __instance)
+        {
+            Transform firemodetext = __instance.transform.Find("AmmoPanel");
+            ConfigHUDController controller = __instance.GetOrAddComponent<ConfigHUDController>();
+            controller.Initialize2(firemodetext);
+        }
+    }
+}
