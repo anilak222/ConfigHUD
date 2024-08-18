9da4f1dd75bf4a55f993527c659034fc14f31df0
anilak222 <k.anastasov034@gmail.com>
2024-08-18 23:11:40 +0100

    Add files via upload

---
+++ ConfigHUDPatch3.cs
@@ -0,0 +1,22 @@
+﻿using EFT;
+using EFT.UI;
+using HarmonyLib;
+using JetBrains.Annotations;
+using SPT.Reflection.Patching;
+using System.Reflection;
+using UnityEngine;
+
+namespace ConfigHUD
+{
+    internal class ConfigHUDPatch3 : ModulePatch
+    {
+        protected override MethodBase GetTargetMethod() => AccessTools.Method(typeof(LocalGame), "Stop");
+
+        public static bool GameHasStopped = false;
+
+        public static void Prefix()
+        {
+            GameHasStopped = true;
+        }
+    }
+}
